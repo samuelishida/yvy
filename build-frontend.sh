4c5ebbc9518128de5f25ba177e226e9c584a7e4d
Samuel Ishida <samuel.toyoshi@gmail.com>
2024-10-20 00:21:11 -0300

    frontend bash

--- build-frontend.sh
+++ build-frontend.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
-#
+
+# Definir possíveis caminhos para o arquivo npmrc
+NPMRC_PATHS=(
+  "$HOME/.npmrc"
+  "/etc/npmrc"
+)
+
+# Loop para verificar cada caminho
+for NPMRC_PATH in "${NPMRC_PATHS[@]}"; do
+  # Verificar se o arquivo existe
+  if [ -f "$NPMRC_PATH" ]; then
+    echo "Arquivo npmrc encontrado em: $NPMRC_PATH"
+    
+    # Verificar se o arquivo contém a linha "production=false"
+    if grep -q "production=false" "$NPMRC_PATH"; then
+      echo "Removendo a linha 'production=false'..."
+      
+      # Usar sed para remover a linha contendo "production=false"
+      sed -i '/production=false/d' "$NPMRC_PATH"
+      
+      echo "Linha removida com sucesso de $NPMRC_PATH."
+    else
+      echo "A linha 'production=false' não foi encontrada em $NPMRC_PATH."
+    fi
+  else
+    echo "Arquivo npmrc não encontrado em: $NPMRC_PATH"
+  fi
+done
 
 # Definir o diretório base do frontend
 FRONTEND_DIR="./frontend"
@@ -28,4 +55,3 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Build do frontend concluído com sucesso!"
-
