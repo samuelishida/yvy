a915ca5522723d48b91964f2b40b88ac494736d4
Samuel Ishida <samuel.toyoshi@gmail.com>
2024-10-20 16:30:14 -0300

    home add new maps

--- node/src/home.js
+++ node/src/home.js
@@ -18,9 +18,11 @@ const Home = () => {
         <button className="btn btn-custom btn-lg mb-3" onClick={() => showIframe('iframe-temperature')}>Temperatura OpenWeatherMap</button>
         <button className="btn btn-custom btn-lg mb-3" onClick={() => showIframe('iframe-terrabrasilis')}>TerraBrasilis</button>
         <button className="btn btn-custom btn-lg mb-3" onClick={() => showIframe('iframe-sea-level')}>Nível do Mar 80 anos</button>
-        <button className="btn btn-custom btn-lg mb-3" onClick={() => showIframe('iframe-earthquakes')}>Terremotos USGS</button>
-        <button className="btn btn-custom btn-lg mb-3" onClick={() => showIframe('iframe-fires')}>Incêndios Florestais NASA FIRMS</button>
         <button className="btn btn-custom btn-lg mb-3" onClick={() => showIframe('iframe-windy')}>Alertas Meteorológicos Windy</button>
+        <button className="btn btn-custom btn-lg mb-3" onClick={() => window.open('https://firms.modaps.eosdis.nasa.gov/map/', '_blank')}>
+          Incêndios Florestais NASA FIRMS
+        </button>
+
       </div>
 
       <div id="iframe-air-quality" className="iframe-container" style={{ display: activeIframe === 'iframe-air-quality' ? 'block' : 'none' }}>
@@ -43,17 +45,18 @@ const Home = () => {
         <iframe src="https://coastal.climatecentral.org/embed/map/10/-43.3654/-22.7935/?theme=water_level&map_type=water_level_above_mhhw&basemap=roadmap&contiguous=true&elevation_model=best_available&water_level=1.0&water_unit=m" title="Climate Central | Land below 1.0 meters of water" width="100%" height="600px" style={{ border: 'none' }} />
       </div>
 
-      <div id="iframe-earthquakes" className="iframe-container" style={{ display: activeIframe === 'iframe-earthquakes' ? 'block' : 'none' }}>
-        <iframe src="https://earthquake.usgs.gov/earthquakes/map/?extent=-43.2932,-107.3584&extent=7.01367,-8.9209" title="Mapa de Terremotos USGS" width="100%" height="600px" style={{ border: 'none' }} />
-      </div>
-
-      <div id="iframe-fires" className="iframe-container" style={{ display: activeIframe === 'iframe-fires' ? 'block' : 'none' }}>
-        <iframe src="https://firms.modaps.eosdis.nasa.gov/map/" title="Mapa de Incêndios Florestais NASA FIRMS" width="100%" height="600px" style={{ border: 'none' }} />
-      </div>
-
       <div id="iframe-windy" className="iframe-container" style={{ display: activeIframe === 'iframe-windy' ? 'block' : 'none' }}>
-        <iframe src="https://www.windy.com/?-22.952,-43.212,5" title="Windy - Mapa Meteorológico" width="100%" height="600px" style={{ border: 'none' }} />
+        <iframe 
+          src="https://embed.windy.com/embed2.html?lat=-22.952&lon=-43.212&detailLat=-22.952&detailLon=-43.212&width=650&height=450&zoom=5&level=surface&overlay=wind&product=ecmwf&menu=&message=true&marker=true&calendar=now&pressure=true&type=map&location=coordinates&detail=true&metricWind=default&metricTemp=default&radarRange=-1" 
+          title="Windy - Mapa Meteorológico" 
+          width="100%" 
+          height="600px" 
+          style={{ border: 'none' }} 
+        />
       </div>
+
+
+
     </div>
   );
 };
